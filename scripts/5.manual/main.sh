776c17dff46f997c0bbd342f1e219e457756d058
Luiz F. C. Martins <239121271+luiz1361@users.noreply.github.com>
2026-01-28 22:50:47 +0000

    Initial commit

---
+++ scripts/5.manual/main.sh
@@ -0,0 +1,5 @@
+# Install gh-dash extension for GitHub CLI
+gh extension install dlvhdr/gh-dash
+
+# Follow the instructions at the link below to get started with aws-sso-cli:
+echo "https://synfinatic.github.io/aws-sso-cli/latest/quickstart/"
