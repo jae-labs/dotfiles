cfa28f82738e62defed06ba2cb5421cf927480e4
Luiz F. C. Martins <239121271+luiz1361@users.noreply.github.com>
2026-08-31 23:21:53 +0100

    feat: implement dreams and technical debt

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Conceal level
+-- Controls how text with the "conceal" syntax attribute is shown.
+-- 0 = show all text normally (nothing hidden)
+-- LazyVim sets this to 2 by default, which hides markdown syntax like ``` and **
+-- Default: 0
+vim.opt.conceallevel = 0
+
 -- Shell configuration
 -- Sets the shell to use for executing commands.
 -- Default: $SHELL or "sh"
